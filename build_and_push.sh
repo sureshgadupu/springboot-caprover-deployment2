714a3b7f56ddc0215c556a3ea525dde30c316d80
sureshgadupu <sureshgkhyd@gmail.com>
2023-01-13 02:49:44 +1300

    GitHub action updated

--- build_and_push.sh
+++ build_and_push.sh
@@ -13,7 +13,7 @@ else
 fi
 
 
-cd $CONTEXT_DIR
+#cd $CONTEXT_DIR
 env
 # ls -lah
 
@@ -23,7 +23,7 @@ env > /tmp/build_args
 echo "--build-arg \""`cat /tmp/build_args | sed -z 's/\n/" --build-arg "/g'`"IGNORE_VAR=IGNORE_VAR\"" >  /tmp/build_args
 
 BUILD_ARGS=`cat /tmp/build_args`
-COMMAND="docker buildx  build  --platform linux/amd64,linux/arm64 --push -t $FULL_IMAGE_NAME -t $IMAGE_NAME_WITH_REGISTRY:latest . -f $DOCKERFILE $BUILD_ARGS --no-cache ."
+COMMAND="docker buildx  build  --platform linux/amd64,linux/arm64 --push -t $FULL_IMAGE_NAME -t $IMAGE_NAME_WITH_REGISTRY:latest -f $DOCKERFILE $BUILD_ARGS --no-cache ."
 
 echo "-------------------"
 echo "-------------------"
